640fab6d5e1c9898814cd7bc85b09f7d0690fa4a
Onojeta Brown <onosbrown.saved@gmail.com>
2022-10-25 07:53:54 +0100

    update

---
+++ src/routes/login/+page.ts
@@ -0,0 +1,12 @@
+import { browser } from "$app/environment";
+import { goto } from "$app/navigation";
+import { auth } from "$lib/helpers/store";
+
+export function load({ params }) {
+    // auth.subscribe(({ loggedIn, cred }) => {
+    //     if (loggedIn && browser) {
+    //         console.log(loggedIn)
+            
+    //     }
+    // });
+}
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-// import adapter from "@sveltejs/adapter-auto";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,8 +8,7 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter(),
-    trailingSlash: 'always',
+	  adapter: adapter(),
 	  prerender: {
 		  enabled:false,
 	  }
